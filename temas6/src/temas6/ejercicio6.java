e59816af6bc248a080eb3c8b2499426dc95d9966
DAM <DAM@XA13D1>
2020-03-12 13:41:08 +0100

    otro creo texto ejercicio 6

--- temas6/src/temas6/ejercicio6.java
+++ temas6/src/temas6/ejercicio6.java
@@ -1,10 +1,18 @@
 package temas6;
 
+import java.util.Scanner;
+
 public class ejercicio6 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		Scanner sc = new Scanner(System.in);
+		double numero = 0;
+		int i=0;
+		for (i=0;i<19; i++ ) {
+			numero =  Math.random();
+			System.out.println("El número aleatorio guardado en la posición v["+i+"] "+numero);
+		}
 	}
 
 }
